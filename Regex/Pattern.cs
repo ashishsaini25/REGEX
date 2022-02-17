b90f4daff8bdbcca8329fde385ef6d13535119b6
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-18 00:04:08 +0530

    [Ashish] Add:First Name Pattern Added

---
+++ Regex/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public  class Pattern
+    {
+        public static string firstname = "^[A-Z]{1}[a-z,A-Z,0-9]{2,}$";
+        public bool Validate(string Firstname)
+        {
+            return Regex.IsMatch(Firstname, firstname);
+        }
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            bool ok= pattern.Validate("p17yggjvkj");
+            Console.WriteLine(ok);  
+        }
+    }
+}
